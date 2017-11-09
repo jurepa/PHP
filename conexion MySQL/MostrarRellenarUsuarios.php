01629efe5bb0c023fc55e4a477021337eed1dc8c
PJarana <pablojosejarana21@gmail.com>
2017-11-09 09:19:04 +0100

    up

---
+++ conexion MySQL/MostrarRellenarUsuarios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Churruca School</title>
+</head>
+<h1><a>Colegio Churruca</a></h1>
+<hr width="100%">
+<body>
+<form action="sentenciasPreparadas.php" method="post">
+    <?php
+    $numUsuarios=$_POST["numUsuarios"]; //Para volver a enviar el num de alumnos: <input type="Hidden" name="numAlumno" value="$_POST["numAlumnos"]">
+    $i=0;
+    while($i<$numUsuarios)
+    {
+        ?>
+        <p>Nombre Usuario: <input type="text" name="nombreUsuario[]">         Apellidos Usuario: <input type="text" name="apellidoUsuario[]"></p><br>
+        <br/><p>Edad: <input type="number" name="edadUsuario[]"></p><br/>
+        <?php
+        $i++;
+    }
+
+    ?>
+    <input type="hidden" name="numUsuarios" value="$numUsuarios" />
+    <input type="submit" name="enviar">
+</form>
+<!-- Falta botón enviar-->
+</body>
+</html>
--- conexion MySQL/sentenciasPreparadas.php
+++ conexion MySQL/sentenciasPreparadas.php
@@ -10,15 +10,21 @@ if ($conexion->connect_error)
 {
     trigger_error("Failed to connect to MySQL: " . $conexion->connect_error, E_USER_ERROR);
 }
-
-$sentenciaPreparada=$conexion->prepare('SELECT ID,Nombre,Apellidos,Edad FROM Usuario WHERE ID>?');
-$sentenciaPreparada->bind_param('i',$numID);
-$numID=1;
-$sentenciaPreparada->execute();
-$result=$sentenciaPreparada->get_result();
-while($row=$result->fetch_assoc())
+$numUsuarios=$_POST["numUsuarios"];
+$nombreUsuario=$_POST["nombreUsuario"];
+$apellidoUsuario=$_POST["apellidoUsuario"];
+$edadUsuario=$_POST["edadUsuario"];
+for($i=0;$i<$numUsuarios;$i++)
 {
-    echo "<li>ID usuario: " . $row["ID"]. " - Nombre: " . $row["Nombre"]. " - Apellidos: " .$row["Apellidos"]. " - Edad: " .$row["Edad"]."</li><br>";
+    $sentenciaPreparada=$conexion->prepare('INSERT INTO Usuario (Nombre,Apellidos,Edad) VALUES (' . $nombreUsuario[$i]. ', ' .$apellidoUsuario[$i]. ', ' .$edadUsuario[$i]. ')');
+    $sentenciaPreparada->execute();
+    $consultaUltimoInsert=$conexion->prepare('SELECT ID,Nombre,Apellidos,Edad FROM Usuario WHERE ID=(SELECT MAX(ID)FROM Usuario)');
+    $result=$consultaUltimoInsert->get_result();
+    while($row=$result->fetch_assoc())
+    {
+        echo "<li>ID usuario: " . $row["ID"]. " - Nombre: " . $row["Nombre"]. " - Apellidos: " .$row["Apellidos"]. " - Edad: " .$row["Edad"]."</li><br>";
+    }
+    $i++;
 }
 
 
